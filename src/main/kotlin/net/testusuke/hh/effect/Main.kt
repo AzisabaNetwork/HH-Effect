d503ddf6e6377590574f86337f75c9aa84657600
testusuke <silverbro007@icloud.com>
2021-03-14 14:16:10 +0900

    [Update] add Totem

--- src/main/kotlin/net/testusuke/hh/effect/Main.kt
+++ src/main/kotlin/net/testusuke/hh/effect/Main.kt
@@ -1,6 +1,7 @@
 package net.testusuke.hh.effect
 
 import net.testusuke.hh.effect.item.record.RecordListener
+import net.testusuke.hh.effect.item.totem.ActivateTotem
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -21,7 +22,7 @@ class Main: JavaPlugin() {
         val listeners = arrayListOf<Listener>() //  Listener List
         ////    Item
         listeners.add(RecordListener)   //  Recode
-
+        listeners.add(ActivateTotem)    //  Totem
         ////  register events
         listeners.forEach { listener ->
             server.pluginManager.registerEvents(listener,this)
